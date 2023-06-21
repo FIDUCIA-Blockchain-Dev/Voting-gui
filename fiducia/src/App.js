eda1ad4370437008c58dfd15fd67be661b6c924a
Lakkaraju Yatin <lakkarajuyatin@gmail.com>
2023-06-21 15:27:26 +0530

    hello world with addresses

--- fiducia/src/App.js
+++ fiducia/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: ''
+    };
+  }
+
+  componentDidMount() {
+    this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    try {
+      // Check if Web3 provider is available from Metamask or similar extension
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.enable(); // Request user permission to connect
+        const accounts = await web3.eth.getAccounts();
+        this.setState({ account: accounts[0] });
+      } else {
+        console.log('Please install MetaMask or use a compatible browser extension.');
+      }
+    } catch (error) {
+      console.error('Error loading blockchain data:', error);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <p>Your account: {this.state.account}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
+
