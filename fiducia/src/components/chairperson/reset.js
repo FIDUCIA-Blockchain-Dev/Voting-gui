c6e3eb35c92e087d37cf2889cd716acae29d2f7e
yatinlakkaraju123 <lakkarajuyatin@gmail.com>
2023-07-16 20:25:41 +0530

    added reset

--- fiducia/src/components/chairperson/reset.js
+++ fiducia/src/components/chairperson/reset.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
-
+import { ABI, address } from '../config';
 
 class App extends Component {
   constructor(props) {
@@ -22,6 +22,8 @@ class App extends Component {
         await window.ethereum.enable(); // Request user permission to connect
         const accounts = await web3.eth.getAccounts();
         this.setState({ account: accounts[0] });
+        const scontract = new web3.eth.Contract(ABI, address)
+        this.setState({ scontract })
       } else {
         console.log('Please install MetaMask or use a compatible browser extension.');
       }
@@ -29,12 +31,19 @@ class App extends Component {
       console.error('Error loading blockchain data:', error);
     }
   }
-
+  async reset() {
+    const {scontract,account} = this.state;
+    await scontract.methods.reset().send({from:account});
+  }
   render() {
     return (
       <div>
         <h1>RESET PAGE</h1>
         <p>Your account: {this.state.account}</p>
+        <div className='container'>
+        <button type="button" class="btn btn-success" onClick={()=>this.reset()} style={{marginRight:'40px'}}>Reset</button>
+        
+        </div>
       </div>
     );
   }
